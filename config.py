584e2c465655a4ab279d0cd60df19ed78b635c4c
egostrange316-debug <egostrange316@gmail.com>
2026-02-17 01:37:54 +0500

    Create config.py with bot configuration

---
+++ config.py
@@ -0,0 +1,7 @@
+# Bot Configuration
+
+BOT_TOKEN = 'your_bot_token_here'
+ADMIN_USERNAME = 'your_admin_username_here'
+ADMIN_IDS = [123456789, 987654321]
+DATABASE_PATH = 'path/to/your/database.db'
+ALLOWED_UPDATES = ['message', 'edited_message', 'channel_post', 'edited_channel_post']
