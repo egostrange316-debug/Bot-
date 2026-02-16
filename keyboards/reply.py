ce1a92ccb8ea20fe4be3dfcbf7222e51b4f97759
egostrange316-debug <egostrange316@gmail.com>
2026-02-17 01:39:28 +0500

    Create keyboards/reply.py file with keyboard layouts for main menu, back menu, back button, share contact, premium plans, and admin panel

---
+++ keyboards/reply.py
@@ -0,0 +1,38 @@
+# Keyboard Layouts
+
+## Main Menu
+main_menu_keyboard = {
+    'one_time_keyboard': True,
+    'keyboard': [
+        [{'text': 'Back'}],
+        [{'text': 'Share Contact'}, {'text': 'Premium Plans'}, {'text': 'Admin Panel'}]
+    ]
+}
+
+## Back Menu
+back_menu_keyboard = {
+    'one_time_keyboard': True,
+    'keyboard': [
+        [{'text': 'Back'}]
+    ]
+}
+
+## Back Button
+back_button = {
+    'text': 'Back'
+}
+
+## Share Contact
+share_contact_button = {
+    'text': 'Share Contact'
+}
+
+## Premium Plans
+premium_plans_button = {
+    'text': 'Premium Plans'
+}
+
+## Admin Panel
+admin_panel_button = {
+    'text': 'Admin Panel'
+}
